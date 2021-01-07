268abc24dc3b0083ae8468e96c57a19239a75f87
HerilS <herilsaha06@gmail.com>
2021-01-06 19:27:03 -0700

    TOS, privacy policy, and favicon

--- pages/privacypolicy.js
+++ pages/privacypolicy.js
@@ -7,9 +7,11 @@ const useStyles = makeStyles((theme) =>
     root: {
       fontFamily: "Gilroy, sans-serif",
       background: "white",
-      minHeight: "100vh",
       position: "realtive",
       zIndex: 1,
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
     },
     circle1: {
       background: "#F8BB86",
@@ -21,6 +23,10 @@ const useStyles = makeStyles((theme) =>
       top: "10rem",
       left: "-7.5rem",
       zIndex: 1,
+      [theme.breakpoints.down("800")]: {
+        width: "16rem",
+        height: "16rem",
+      },
     },
     circle2: {
       background: "transparent",
@@ -33,6 +39,41 @@ const useStyles = makeStyles((theme) =>
       right: "5rem",
       bottom: "5rem",
       zIndex: 1,
+      [theme.breakpoints.down("800")]: {
+        right: "-5rem",
+        width: "16rem",
+        height: "16rem",
+      },
+      [theme.breakpoints.down("500")]: {
+        opacity: "5%",
+      },
+    },
+    content: {
+      maxWidth: "70rem",
+      marginTop: "10rem",
+      marginBottom: "10rem",
+      marginLeft: "3.5rem",
+      marginRight: "3.5rem",
+      "& a": {
+        color: "#03a9f4",
+        fontWeight: "bold",
+      },
+      "& div": {
+        fontSize: "6rem",
+        lineHeight: "6rem",
+        fontWeight: "bold",
+        marginBottom: "1.5rem",
+        [theme.breakpoints.down("500")]: {
+          fontSize: "4rem",
+          lineHeight: "4.5rem",
+        },
+      },
+      "& p": {
+        fontSize: "1.2rem",
+        [theme.breakpoints.down("500")]: {
+          fontSize: "1rem",
+        },
+      },
     },
   })
 );
@@ -43,8 +84,51 @@ export default function privacypolicy() {
       <div className={classes.root}>
         <div className={classes.circle1}></div>
         <div className={classes.circle2}></div>
-        <div>
-            hello
+
+        <div className={classes.content}>
+          <div>Privacy Policy</div>
+          <p>
+            Your privacy is important to us. It is Bikiology Inc.'s policy to
+            respect your privacy regarding any information we may collect from
+            you across our website,{" "}
+            <a href="https://www.bikiology.com">https://www.bikiology.com</a>,
+            and other sites we own and operate.
+          </p>
+          <p>
+            We only ask for personal information when we truly need it to
+            provide a service to you. We collect it by fair and lawful means,
+            with your knowledge and consent. We also let you know why we’re
+            collecting it and how it will be used.
+          </p>
+          <p>
+            We only retain collected information for as long as necessary to
+            provide you with your requested service. What data we store, we’ll
+            protect within commercially acceptable means to prevent loss and
+            theft, as well as unauthorized access, disclosure, copying, use or
+            modification.
+          </p>
+          <p>
+            We don’t share any personally identifying information publicly or
+            with third-parties, except when required to by law.
+          </p>
+          <p>
+            Our website may link to external sites that are not operated by us.
+            Please be aware that we have no control over the content and
+            practices of these sites, and cannot accept responsibility or
+            liability for their respective privacy policies.
+          </p>
+          <p>
+            You are free to refuse our request for your personal information,
+            with the understanding that we may be unable to provide you with
+            some of your desired services.
+          </p>
+          <p>
+            Your continued use of our website will be regarded as acceptance of
+            our practices around privacy and personal information. If you have
+            any questions about how we handle user data and personal
+            information, feel free to contact us.
+          </p>
+          <p>This policy is effective as of 6 January 2021.</p>
         </div>
       </div>
     </Layout>
--- pages/terms-and-conditions.js
+++ pages/terms-and-conditions.js
@@ -1,9 +1,215 @@
-import React from 'react'
+import React from "react";
+import Layout from "../components/layout";
+import { makeStyles, createStyles } from "@material-ui/core/styles";
 
-export default function termsandconditions() {
-    return (
-        <div>
-            
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    root: {
+      fontFamily: "Gilroy, sans-serif",
+      background: "white",
+      position: "realtive",
+      zIndex: 1,
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    circle1: {
+      background: "#F8BB86",
+      width: "22.5rem",
+      height: "22.5rem",
+      position: "absolute",
+      borderRadius: "50%",
+      opacity: "10%",
+      top: "10rem",
+      left: "-7.5rem",
+      zIndex: 1,
+      [theme.breakpoints.down("800")]: {
+        width: "16rem",
+        height: "16rem",
+      },
+    },
+    circle2: {
+      background: "transparent",
+      width: "22.5rem",
+      height: "22.5rem",
+      position: "absolute",
+      borderRadius: "50%",
+      opacity: "10%",
+      boxShadow: "0 0 0 0.5rem #F8BB86",
+      right: "5rem",
+      bottom: "5rem",
+      zIndex: 1,
+      [theme.breakpoints.down("800")]: {
+        right: "-5rem",
+        width: "16rem",
+        height: "16rem",
+      },
+      [theme.breakpoints.down("500")]: {
+        opacity: "5%",
+      },
+    },
+    content: {
+      maxWidth: "70rem",
+      marginTop: "10rem",
+      marginBottom: "10rem",
+      marginLeft: "3.5rem",
+      marginRight: "3.5rem",
+      "& a": {
+        color: "#03a9f4",
+        fontWeight: "bold",
+      },
+      "& div": {
+        fontSize: "6rem",
+        lineHeight: "6rem",
+        fontWeight: "bold",
+        marginBottom: "1.5rem",
+        [theme.breakpoints.down("500")]: {
+          fontSize: "3rem",
+          lineHeight: "3.5rem",
+        },
+      },
+      "& p": {
+        fontSize: "1.2rem",
+        [theme.breakpoints.down("500")]: {
+          fontSize: "1rem",
+        },
+      },
+      "& h3": {
+        fontSize: "1.75rem",
+        [theme.breakpoints.down("500")]: {
+          fontSize: "1.25rem",
+        },
+      },
+      "& ol": {
+        fontSize: "1.2rem",
+        [theme.breakpoints.down("500")]: {
+          fontSize: "1rem",
+        },
+      },
+    },
+  })
+);
+export default function terms() {
+  const classes = useStyles();
+  return (
+    <Layout>
+      <div className={classes.root}>
+        <div className={classes.circle1}></div>
+        <div className={classes.circle2}></div>
+
+        <div className={classes.content}>
+          <div>Bikiology Inc. Terms of Service</div>
+          <h3>1. Terms</h3>
+          <p>
+            By accessing the website at{" "}
+            <a href="https://www.bikiology.com">https://www.bikiology.com</a>,
+            you are agreeing to be bound by these terms of service, all
+            applicable laws and regulations, and agree that you are responsible
+            for compliance with any applicable local laws. If you do not agree
+            with any of these terms, you are prohibited from using or accessing
+            this site. The materials contained in this website are protected by
+            applicable copyright and trademark law.
+          </p>
+          <h3>2. Use License</h3>
+          <ol type="a">
+            <li>
+              Permission is granted to temporarily download one copy of the
+              materials (information or software) on Bikiology Inc.'s website
+              for personal, non-commercial transitory viewing only. This is the
+              grant of a license, not a transfer of title, and under this
+              license you may not:
+              <ol type="i">
+                <li>modify or copy the materials;</li>
+                <li>
+                  use the materials for any commercial purpose, or for any
+                  public display (commercial or non-commercial);
+                </li>
+                <li>
+                  attempt to decompile or reverse engineer any software
+                  contained on Bikiology Inc.'s website;
+                </li>
+                <li>
+                  remove any copyright or other proprietary notations from the
+                  materials; or
+                </li>
+                <li>
+                  transfer the materials to another person or "mirror" the
+                  materials on any other server.
+                </li>
+              </ol>
+            </li>
+            <li>
+              This license shall automatically terminate if you violate any of
+              these restrictions and may be terminated by Bikiology Inc. at any
+              time. Upon terminating your viewing of these materials or upon the
+              termination of this license, you must destroy any downloaded
+              materials in your possession whether in electronic or printed
+              format.
+            </li>
+          </ol>
+          <h3>3. Disclaimer</h3>
+          <ol type="a">
+            <li>
+              The materials on Bikiology Inc.'s website are provided on an 'as
+              is' basis. Bikiology Inc. makes no warranties, expressed or
+              implied, and hereby disclaims and negates all other warranties
+              including, without limitation, implied warranties or conditions of
+              merchantability, fitness for a particular purpose, or
+              non-infringement of intellectual property or other violation of
+              rights.
+            </li>
+            <li>
+              Further, Bikiology Inc. does not warrant or make any
+              representations concerning the accuracy, likely results, or
+              reliability of the use of the materials on its website or
+              otherwise relating to such materials or on any sites linked to
+              this site.
+            </li>
+          </ol>
+          <h3>4. Limitations</h3>
+          <p>
+            In no event shall Bikiology Inc. or its suppliers be liable for any
+            damages (including, without limitation, damages for loss of data or
+            profit, or due to business interruption) arising out of the use or
+            inability to use the materials on Bikiology Inc.'s website, even if
+            Bikiology Inc. or a Bikiology Inc. authorized representative has
+            been notified orally or in writing of the possibility of such
+            damage. Because some jurisdictions do not allow limitations on
+            implied warranties, or limitations of liability for consequential or
+            incidental damages, these limitations may not apply to you.
+          </p>
+          <h3>5. Accuracy of materials</h3>
+          <p>
+            The materials appearing on Bikiology Inc.'s website could include
+            technical, typographical, or photographic errors. Bikiology Inc.
+            does not warrant that any of the materials on its website are
+            accurate, complete or current. Bikiology Inc. may make changes to
+            the materials contained on its website at any time without notice.
+            However Bikiology Inc. does not make any commitment to update the
+            materials.
+          </p>
+          <h3>6. Links</h3>
+          <p>
+            Bikiology Inc. has not reviewed all of the sites linked to its
+            website and is not responsible for the contents of any such linked
+            site. The inclusion of any link does not imply endorsement by
+            Bikiology Inc. of the site. Use of any such linked website is at the
+            user's own risk.
+          </p>
+          <h3>7. Modifications</h3>
+          <p>
+            Bikiology Inc. may revise these terms of service for its website at
+            any time without notice. By using this website you are agreeing to
+            be bound by the then current version of these terms of service.
+          </p>
+          <h3>8. Governing Law</h3>
+          <p>
+            These terms and conditions are governed by and construed in
+            accordance with the laws of Canada and you irrevocably submit to the
+            exclusive jurisdiction of the courts in that State or location.
+          </p>
         </div>
-    )
+      </div>
+    </Layout>
+  );
 }
